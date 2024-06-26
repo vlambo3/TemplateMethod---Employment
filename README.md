1378c4971c1356ab3dad7b67185c150a38e28b2c
vlambo3 <contadora.godoy.vanina@gmail.com>
2024-06-25 21:34:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Explanation of the Program in the Main Branch
+
+Our program is responsible for managing employee salary liquidation. To accomplish this task, we follow a specific process involving three key steps:
+
+### 1. Calculate the Corresponding Salary:
+- For permanent employees, the salary is calculated based on a basic salary, with applicable discounts and bonuses.
+- For contract employees, the salary is calculated based on the number of hours worked and the specific hourly rate.
+
+### 2. Print or Generate Salary Receipts:
+- Permanent employees receive a printed salary receipt.
+- Contract employees receive a digital salary receipt.
+
+### 3. Deposit the Corresponding Amount:
+- Regardless of the employee type, the calculated salary is deposited into the employee's bank account.
+
+In summary, the process we perform is called `calculateTotalSalary()`, which includes the steps of calculation, printing, and depositing the salary. Each employee has a name, surname, and a bank account number used for salary deposits. Permanent employees have additional information such as basic salary, discounts, and bonuses, while contract employees have the number of hours worked and the hourly rate.
+
+## Usage of the Template Method Design Pattern in the Template Method Branch
+
+In the `template-method` branch, we have implemented the Template Method design pattern to simplify and better structure the salary liquidation process. This pattern allows us to define the skeleton of an algorithm in a base class and delegate the implementation of specific steps to subclasses, promoting code reuse and extensibility.
---
+++ src/EmployeeTest.java
@@ -0,0 +1,110 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    private Employee effectiveEmployee;
+    private Employee contractorEmployee;
+    private Employee.Calculate.CalculateEffective calculateEffective;
+    private Employee.Calculate.CalculateContractor calculateContractor;
+    private Employee effectiveEmployeeException;
+    private Employee contractorEmployeeException;
+
+    @BeforeEach
+    void setUp() {
+        effectiveEmployee = new Employee.Effective("Juan", "Perez", "aaa111", 400d, 40d, 60d);
+        contractorEmployee = new Employee.Contractor("Juan", "Perez", "aaa111", 201d, 40d);
+        calculateEffective = new Employee.Calculate.CalculateEffective();
+        calculateContractor = new Employee.Calculate.CalculateContractor();
+
+        effectiveEmployeeException = new Employee.Effective("Juan", "Perez", "123456", 1000.0, 2000.0, 50.0);
+        contractorEmployeeException = new Employee.Contractor("Pompilia", "Pompini", "654321", -10.0, 100.0);
+
+    }
+
+    @Test
+    @DisplayName("Ok calculate Effective")
+    void testCalculateEffectiveOk() {
+        //arrange
+        String expectedResponse = "La liquidación generada es: un documento impreso. Saldo a liquidar: 420.0";
+
+        //act
+        String testResponse = calculateEffective.calculateTotalSalary(effectiveEmployee);
+
+        //assert
+        assertEquals(expectedResponse, testResponse);
+    }
+
+    @Test
+    @DisplayName("Error when print")
+    void testCalculateEffectiveErrorPrint() {
+        //arrange
+        String expectedResponse = "La liquidación generada es: un documento digital. Saldo a liquidar: 420.0";
+
+        //act
+        String testResponse = calculateEffective.calculateTotalSalary(effectiveEmployee);
+
+        //assert
+        assertNotEquals(expectedResponse, testResponse);
+    }
+
+    @Test
+    @DisplayName("Error when select an employee")
+    void testCalculateEffectiveErrorEmployee() {
+        //arrange
+        String expectedResponse = "La liquidación generada es: un documento impreso. Saldo a liquidar: 420.0";
+
+        //act
+        String testResponse = calculateContractor.calculateTotalSalary(effectiveEmployee);
+
+        //assert
+        assertNotEquals(expectedResponse, testResponse);
+    }
+
+    @Test
+    @DisplayName("Error when print")
+    void testCalculateContractorErrorPrint() {
+        //arrange
+        String expectedResponse = "La liquidación generada es: un documento impreso. Saldo a liquidar: 8040.0";
+
+        //act
+        String testResponse = calculateContractor.calculateTotalSalary(contractorEmployee);
+
+        //assert
+        assertNotEquals(expectedResponse, testResponse);
+    }
+
+    @Test
+    @DisplayName("Ok calculate Contractor")
+    void testCalculateContractor() {
+        //arrange
+        String expectedResponse = "La liquidación generada es: un documento digital. Saldo a liquidar: 8040.0";
+
+        //act
+        String testResponse = calculateContractor.calculateTotalSalary(contractorEmployee);
+
+        //assert
+        assertNotEquals(expectedResponse, testResponse);
+    }
+
+    @Test
+    @DisplayName("Throw exception when calculating salary for Effective Employee")
+    void testCalculateSalaryErrorEffective() {
+        // Act & Assert
+        assertThrows(ClassCastException.class, () -> {
+            calculateEffective.calculateTotalSalary(effectiveEmployeeException);
+        });
+    }
+
+    @Test
+    @DisplayName("Throw exception when calculating salary for Contractor Employee")
+    void testCalculateSalaryErrorContractor() {
+        // Act & Assert
+        assertThrows(ClassCastException.class, () -> {
+            calculateContractor.calculateTotalSalary(contractorEmployeeException);
+        });
+    }
+}
